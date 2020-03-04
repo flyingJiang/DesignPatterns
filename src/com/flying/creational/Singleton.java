40489c66d3931a5b1e134b999fbef48870193f34
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-03-04 13:54:45 +0800

    Singleton

---
+++ src/com/flying/creational/Singleton.java
@@ -0,0 +1,15 @@
+package com.flying.creational;
+
+public class Singleton {
+
+    public static Singleton instance = null;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
