40d85954c6f9927dbe41bc1c299086cef2d97b65
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-03-05 14:49:05 +0800

    Prototype Pattern

---
+++ src/com/flying/creational/prototypePattern/Book.java
@@ -0,0 +1,13 @@
+package com.flying.creational.prototypePattern;
+
+public class Book extends Item {
+    private int numberOfPages;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+}
---
+++ src/com/flying/creational/prototypePattern/Prototype.java
@@ -0,0 +1,13 @@
+package com.flying.creational.prototypePattern;
+
+/**
+ *  Concepts
+ *  Avoids costly creation
+ *  Avoids subclassing
+ *  typically doesn't use "new"
+ *  Often utilizes an Interface
+ *  Usually implemented with a Registry
+ *  Examlpe: java.lang.Object#clone()
+ */
+public class Prototype {
+}
