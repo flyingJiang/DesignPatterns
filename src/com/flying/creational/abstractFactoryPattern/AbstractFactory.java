387312b34d5b7402dd127de8c8c384900c3c9f4b
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-03-09 17:36:56 +0800

    AbstractFactory

---
+++ src/com/flying/creational/abstractFactoryPattern/AbstractFactory.java
@@ -0,0 +1,11 @@
+package com.flying.creational.abstractFactoryPattern;
+
+/**
+ * Factory if Factories
+ * Factory of related objects
+ * Common Interface
+ * Defer to Subclasses
+ * Examples: DocumentBuilder, Frameworks
+ */
+public class AbstractFactory {
+}
