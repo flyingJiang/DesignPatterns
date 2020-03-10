be5e992cc3922da1985fde69e974803a0bd86e33
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-03-10 18:08:00 +0800

    Adapter

--- src/com/flying/structural/adapter/Adapter.java
+++ src/com/flying/structural/adapter/Adapter.java
@@ -1,4 +1,21 @@
 package com.flying.structural.adapter;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Plug adapter
+ * Convert interface into another interface
+ * Legacy
+ * Translates requests
+ * Client Adapter Adaptee
+ * Examples: Arrays Lists
+ */
 public class Adapter {
+    public static void main(String[] args) {
+        Integer[] ints = new Integer[]{1,2,3};
+        List<Integer> list = Arrays.asList(ints);
+        System.out.println(ints);
+        System.out.println(list);
+    }
 }
