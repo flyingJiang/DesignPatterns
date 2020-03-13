505a55cf3d3e70c99e51499646e5fca71fdb0913
Flying.Jiang <Flying.Jiang@homecredit.cn>
2020-03-13 10:39:56 +0800

    Bridge

---
+++ src/com/flying/structural/bridge/Bridge.java
@@ -0,0 +1,14 @@
+package com.flying.structural.bridge;
+
+/**
+ *  Decouple Abstraction and implementation
+ *  Encapsulation, Composition, Inheritance
+ *  Changes in Abstraction won't affect client
+ *  Details won,t be right
+ *  Example: Driver, JDBC
+ */
+public class Bridge {
+    public static void main(String[] args) {
+
+    }
+}
